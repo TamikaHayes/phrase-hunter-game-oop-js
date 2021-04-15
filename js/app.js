4511ea6e58081597fe8ad0903da5a9e3e0eb615b
TamikaHayes <tamika.hayes@gmail.com>
2021-04-14 20:34:55 -0700

    Updated app.js with start button event listener

--- js/app.js
+++ js/app.js
@@ -3,17 +3,16 @@
  * app.js */
 
 
-
-
-const game = new Game();
-
-
 // const game = new Game();
 
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 
-// const randomPhrase = game.getRandomPhrase();
-// const phrase = new Phrase(randomPhrase.phrase);
-// phrase.addPhraseToDisplay();
+let game;
 
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+const button = document.querySelector('#btn__reset');
+
+button.addEventListener('click',(e) => {
+    game = new Game;
+    game.startGame();
+});
